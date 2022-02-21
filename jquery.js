e1736b12f9a842b19936a86a815c97cb70d72502
rakuspHoshi <ryuto.hoshi@rakus-partners.co.jp>
2022-02-21 11:39:22 +0900

    ex05まで終了

--- jquery.js
+++ jquery.js
@@ -9,8 +9,44 @@ $(function(){
     });
   });
 
+  //ex02
   $(function(){
     $('#colorChange').on('click', function() {
       $('#name').toggleClass('chang');
     });
   });
+
+  //ex03
+  
+  $(function(){
+    $('#hobby').on('change', function() {
+      if($('#hobby').val() == '2'){
+        $('.3').css('display', 'none')
+        $('.2').css('display', 'block');
+      } else if($('#hobby').val() == '3') {
+        $('.2').css('display', 'none');
+        $('.3').css('display', 'block');
+      } else {
+        $('.2').css('display', 'none');
+        $('.3').css('display', 'none')
+      }
+    });
+  });
+
+  //ex04
+  $(function () {
+    $('#ex04').submit(function () {
+      $(this).find(':submit').prop('disabled', 'true');
+    });
+  });
+
+  //ex05
+  $(function(){
+    $('#ex05').find(':submit').on('click', function(){
+      if($('#inputEmail').val() === ''){
+        alert('メールアドレスを入力してください！');
+      }
+    });
+  });
+
+  
